305e5be700f729e0d0d12479d0bc960cb17a350a
Shrikrashna Gutte <shrikrashna.gutte@gmail.com>
2024-12-27 11:56:57 +0530

    this is my fisrt commit

---
+++ src/main/java/com/testingshastra/utils/App.java
@@ -0,0 +1,19 @@
+package com.testingshastra.utils;
+
+public class App {
+	static String filePath = System.getProperty("user.dir")+"/src/main/resources/Config/app.properties";
+
+	static final PropUtils prop = new PropUtils();
+	
+	public static String getBrowserName() {
+		return prop.getProperty(filePath, "browser_name");
+	}
+	
+	public static String getAppUrl(String env) {
+		return prop.getProperty(filePath, env+"_app_url");
+	}
+	
+	public static boolean isOnGrid() {
+		return Boolean.parseBoolean(prop.getProperty(filePath, "isOnGrid"));
+	}
+}
